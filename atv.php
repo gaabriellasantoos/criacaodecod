6fafc12d2783b3e090a18217d4205312290bebc1
Gabriella Santos <goncalves.gabriella@alunoescolassociais.org.br>
2024-03-13 09:13:44 -0300

    verificando se o número é par ou ímpar com php

---
+++ atv.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    
+    <?php
+    $num = 4;
+
+    if($num %2 == 0){
+        echo "24 é um número PAR";
+    }else{
+        echo "15 é um número IMPAR";
+    }
+  
+    ?>
+</body>
+</html>
